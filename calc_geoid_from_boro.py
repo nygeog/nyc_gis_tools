b676d30c7a9dccbda3eb4f6e1959d30f6766ba3a
Daniel M Sheehan <danielmsheehan@MacBook.local>
2013-07-10 16:15:55 -0400

    added code

---
+++ calc_geoid_from_boro.py
@@ -0,0 +1,25 @@
+import arcpy
+
+input_tract_file = "C:.......file.shp"
+new_field_name = "geoid"
+field_type = "TEXT"
+
+CalcBorotoCountyExpr  = "boroCountyFIPS(int(!BoroCode!), !CT2010!)"
+CalcBorotoCountyBlock = """def boroCountyFIPS(boro,tract):
+  st = '36'
+  if boro == 1:
+    return st + '061' + tract
+  elif boro == 2:
+    return st + '005' + tract
+  elif boro == 3:
+    return st + '047' + tract
+  elif boro == 4:
+    return st + '081' + tract
+  elif boro == 5:
+    return st + '085' + tract
+  else:
+    return 'X' + tract """
+
+arcpy.AddField_management(input_tract_file,new_field_name,field_type,"#","#","#","#","NULLABLE","NON_REQUIRED","#")
+
+arcpy.CalculateField_management(input_tract_file,new_field_name,,"PYTHON_9.3",CalcBorotoCountyBlock )
