b912c8483b2bcefec7ad67dda83da96bb6a18bd3
nygeog <daniel.martin.sheehan@gmail.com>
2015-03-27 08:34:56 -0400

    update

---
+++ calc-geoid-tract-2000.py
@@ -0,0 +1,27 @@
+import arcpy
+
+print 'create and calc geoid for nyc dcp census 2010 tract and block'
+input_tract_file = "C:.......tract_file.shp"
+new_field_name = "geoid"
+field_type = "TEXT"
+
+CalcBorotoCountyExpr  = "boroCountyFIPS(int(!BoroCode!), !CT2000!)"
+CalcBorotoCountyBlock = """def boroCountyFIPS(boro,tract):
+  st = '36'
+  if boro == 1:
+    return st + '061' + tract
+  elif boro == 2:
+    return st + '005' + tract
+  elif boro == 3:
+    return st + '047' + tract
+  elif boro == 4:
+    return st + '081' + tract
+  elif boro == 5:
+    return st + '085' + tract
+  else:
+    return 'X' + tract """
+
+print 'add field and calc - tract geoid'
+arcpy.AddField_management(input_tract_file,new_field_name,field_type,"#","#","#","#","NULLABLE","NON_REQUIRED","#")
+#arcpy.AddField_management(input_tract_file,new_field_name+"_int)",field_type,"#","#","#","#","NULLABLE","NON_REQUIRED","#")
+arcpy.CalculateField_management(input_tract_file,new_field_name,CalcBorotoCountyExpr, "PYTHON_9.3",CalcBorotoCountyBlock)
