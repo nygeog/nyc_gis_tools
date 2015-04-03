e97601ef22585cc055bfa7c3cb628be3e46543fa
nygeog <daniel.martin.sheehan@gmail.com>
2015-04-03 09:54:16 -0400

    update

--- calc_geoid_from_boro.py
+++ calc_geoid_from_boro.py
@@ -50,3 +50,59 @@ CalcBorotoCountyBlock = """def boroCountyFIPS(boro,tract, block):
 print 'add field and calc - block geoid'
 arcpy.AddField_management(input_block_file,new_field_name,field_type,"#","#","#","#","NULLABLE","NON_REQUIRED","#")
 arcpy.CalculateField_management(input_block_file,new_field_name,CalcBorotoCountyExpr, "PYTHON_9.3",CalcBorotoCountyBlock)
+
+
+
+
+#### Actual Dropbox blocks
+print 'create and calc geoid for nyc dcp census 2010 block'
+input_block_file = "W:/GIS/Data/Municipal/USA/New_York/New_York_City/Census/nycb2010_11a_av/nycb2010.shp"
+new_field_name = "geoid"
+field_type = "TEXT"
+
+CalcBorotoCountyExpr  = "boroCountyFIPS(int(!BoroCode!), !CT2010!, !CB2010!)"
+CalcBorotoCountyBlock = """def boroCountyFIPS(boro,tract, block):
+  st = '36'
+  if boro == 1:
+    return st + '061' + tract + block
+  elif boro == 2:
+    return st + '005' + tract + block
+  elif boro == 3:
+    return st + '047' + tract + block
+  elif boro == 4:
+    return st + '081' + tract + block
+  elif boro == 5:
+    return st + '085' + tract + block
+  else:
+    return 'X' + tract + block"""
+    
+print 'add field and calc - block geoid'
+arcpy.AddField_management(input_block_file,new_field_name,field_type,"#","#","#","#","NULLABLE","NON_REQUIRED","#")
+arcpy.CalculateField_management(input_block_file,new_field_name,CalcBorotoCountyExpr, "PYTHON_9.3",CalcBorotoCountyBlock)
+
+
+
+print 'create and calc geoid for nyc dcp census 2000 block'
+input_block_file = "W:/GIS/Data/Municipal/USA/New_York/New_York_City/Census/nycb2000_11a_av/nycb2000.shp"
+new_field_name = "geoid"
+field_type = "TEXT"
+
+CalcBorotoCountyExpr  = "boroCountyFIPS(int(!BoroCode!), !CT2000!, !CB2000!)"
+CalcBorotoCountyBlock = """def boroCountyFIPS(boro,tract, block):
+  st = '36'
+  if boro == 1:
+    return st + '061' + tract + block
+  elif boro == 2:
+    return st + '005' + tract + block
+  elif boro == 3:
+    return st + '047' + tract + block
+  elif boro == 4:
+    return st + '081' + tract + block
+  elif boro == 5:
+    return st + '085' + tract + block
+  else:
+    return 'X' + tract + block"""
+    
+print 'add field and calc - block geoid'
+arcpy.AddField_management(input_block_file,new_field_name,field_type,"#","#","#","#","NULLABLE","NON_REQUIRED","#")
+arcpy.CalculateField_management(input_block_file,new_field_name,CalcBorotoCountyExpr, "PYTHON_9.3",CalcBorotoCountyBlock)
